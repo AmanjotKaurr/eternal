9d17329c1fd2b26aa22ddc96b7e87820ab7762a9
Amanjot <amanjotkauroff@gmail.com>
2025-04-05 14:16:06 +0530

    second page done

--- src/app/will/page.tsx
+++ src/app/will/page.tsx
@@ -1,10 +1,14 @@
-import { Input } from "@/components/ui/input";
-import React from "react";
+"use client"
 
-const page = () => {
+import React from "react"
+import { Input } from "@/components/ui/input"
+import { DatePickerDemo } from "@/components/ui/date"
+import { ArrowRight } from "lucide-react"
+
+const Page = () => {
   return (
     <div className="min-h-screen w-screen bg-zinc-900 p-4 text-white">
-      <div className="h-[10vh] flex items-center justify-between  border-b-2 border-white/15 px-20 py-10 ">
+      <div className="h-[10vh] flex items-center justify-between border-b-2 border-white/15 px-20 py-10">
         <div className="flex items-center gap-5">
           <h1>WillyFi</h1>
           <div className="bg-gray-600 text-white/40 rounded-full px-4">
@@ -25,27 +29,40 @@ const page = () => {
           </h3>
         </div>
 
-        <div className="h-[80vh] w-[80%] mx-auto bg-gray-500/30 border-white/15 rounded-md mt-6 p-10">
+        <div className="max-h-[80vh] w-[80%] mx-auto bg-gray-500/30 border-white/15 rounded-md mt-6 p-10 mb-0">
           <h1 className="text-2xl font-bold">Create new escrow</h1>
-          <div className="h-[10%] w-[90%] mt-10">
+
+          <div className="h-[10%] w-[90%] mt-10 mb-10">
             <h1 className="text-md font-light">Beneficiary Address</h1>
-            <Input className="py-6 mt-2 " type="text" placeholder="Enter Ethereum address" />
+            <Input className="py-6 mt-2" type="text" placeholder="Enter Ethereum address" />
           </div>
 
           <div className="h-[10%] w-[90%] mt-10">
             <h1 className="text-md font-light">Amount (SOL)</h1>
-            <Input className="py-6 mt-2 " type="number" placeholder="Enter Ethereum address" />
+            <Input className="py-6 mt-2" type="number" placeholder="Enter amount" />
           </div>
-
 
           <div className="h-[10%] w-[90%] mt-10">
-            <h1 className="text-md font-light">Select Deadline Date</h1>
-            <Input className="py-6 mt-2 " type="number" placeholder="Enter Ethereum address" />
+            <h1 className="text-md font-light mb-2">Select Deadline Date</h1>
+            <DatePickerDemo />
           </div>
+
+          <button className="w-[90%] bg-gray-600 mt-8 h-[5vh] rounded-md text-white/25">Create Switch</button>
         </div>
+        <div className="h-[20vh] w-[80%] mx-auto bg-gray-500/30 border-white/15 rounded-md mt-6 p-10">
+        <h1 className="text-2xl font-bold">Your Escrows</h1>
+        <h3 className="text-lg text-white/25 mt-5">No record found</h3>
+        </div>
+
+        <div className="flex gap-3 mx-auto w-[80%] mt-4">
+            <button className="px-[15%] py-2 bg-red-400 text-white font-bold rounded-md">15s Escrow for testing</button>
+            <button className="px-[15%] py-2 bg-red-400 text-white font-bold rounded-md">30s Escrow for testing</button>
+        </div>
+
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default Page
